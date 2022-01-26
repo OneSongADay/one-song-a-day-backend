a63509d42144b9158f83a6360c707a4363a3faa1
Paco Lopez <francisco.lopsan@gmail.com>
2022-01-26 22:27:57 +0100

    Send tweet processed

---
+++ lib/one_song_a_day/twitter.ex
@@ -0,0 +1,18 @@
+defmodule OneSongADay.Twitter do
+  def process_tweet(live_song) do
+    if(live_song.tweet_text == nil) do
+      default_tweet(live_song)
+    else
+      live_song.tweet_text
+    end
+  end
+
+  defp default_tweet(live_song) do
+    "🎵 La canción del día ya disponible en #OneSongADay!!
+    Hoy os traemos #{live_song.title} de #{live_song.author} TODO Twitter autor!!
+
+    Spotify: #{live_song.spotify_link}
+
+    Youtube: https://youtu.be/#{live_song.youtube_link}"
+  end
+end
--- lib/one_song_a_day_web/views/live_song_view.ex
+++ lib/one_song_a_day_web/views/live_song_view.ex
@@ -1,6 +1,7 @@
 defmodule OneSongADayWeb.LiveSongView do
   use OneSongADayWeb, :view
   alias OneSongADayWeb.LiveSongView
+  alias OneSongADay.Twitter
 
   def render("index.json", %{song_of_the_day: live_song}) do
     %{data: render_one(live_song, LiveSongView, "song_of_the_day.json")}
@@ -14,7 +15,7 @@ defmodule OneSongADayWeb.LiveSongView do
       release_date: live_song.release_date,
       spotify_link: live_song.spotify_link,
       youtube_link: live_song.youtube_link,
-      tweet_text: live_song.tweet_text
+      tweet: Twitter.process_tweet(live_song)
     }
   end
 end
