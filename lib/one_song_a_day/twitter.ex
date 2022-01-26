3b31c674363598c6aad119008ff4ab3c7221fe08
Paco Lopez <francisco.lopsan@gmail.com>
2022-01-27 00:36:59 +0100

    Test

--- lib/one_song_a_day/twitter.ex
+++ lib/one_song_a_day/twitter.ex
@@ -1,7 +1,8 @@
 defmodule OneSongADay.Twitter do
   def process_tweet(live_song) do
     if(live_song.tweet_text == nil) do
-      default_tweet(live_song)
+      {:ok, text} = Jason.encode(default_tweet(live_song))
+      text
     else
       live_song.tweet_text
     end
@@ -11,7 +12,7 @@ defmodule OneSongADay.Twitter do
     "🎵 La canción del día ya disponible en #OneSongADay!!
 
     Hoy os traemos \"#{live_song.title}\" de #{live_song.author}#{if live_song.author_twitter do
-      " (\@{live_song.author_twitter})"
+      " (@#{live_song.author_twitter})"
     end}!!
 
     Spotify: #{live_song.spotify_link}
